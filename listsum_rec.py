b069cff1f5b29693797503f54b513b335b956d87
Elhaile Fiseha <hailechof@gmail.com>
2019-11-06 16:29:32 +0100

    creat sumlist recursive.

---
+++ listsum_rec.py
@@ -0,0 +1,17 @@
+
+l = list(range(1, 11))
+## recurive way of list sum
+def somrec(l, start = 0, end = len(l)):
+    if len(l) == start:
+        return 0
+    else:
+       return l[start] + somrec(l[start + 1:end+1])
+## iterative (without recursive) list sum
+def sumlist(l, start = 0, end = len(l)):
+    som = 0
+    for i in range(start, end + 1):
+            som += l[i]
+    return som
+
+print(somrec(l, 2, 6))
+print(sumlist(l, 2, 6))
