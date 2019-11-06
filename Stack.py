85a17a433620f363c82a644577dcb9621b82abd0
Elhaile Fiseha <hailechof@gmail.com>
2019-11-06 14:45:45 +0100

    Create a stack class

---
+++ Stack.py
@@ -0,0 +1,12 @@
+# creating a stack class
+
+class Stack(object):
+    """ Creating a Stack class"""
+    def __init__(self):
+        self._List = []
+    def push(self, x):
+        self._List.append(x)
+    def pop(self):
+        return self._List.pop()
+    def empty(self):
+        return len(self._List) == 0
